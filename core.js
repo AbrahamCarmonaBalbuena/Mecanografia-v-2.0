6eb9b2354d092da870ba2aa01bf97d28c97c161a
samuel carmona <L19320912@acapulco.tcnm.mx>
2021-06-03 10:03:11 -0500

    Modificando el cuadro de texto

--- core.js
+++ core.js
@@ -1,7 +1,21 @@
 let canvas = document.getElementById('canvas'),
     context = canvas.getContext('2d'),
     btnReinicio=document.getElementById('btnReinicio'),
+    posX=50
+    posY=400
 
+    /*dificultad = {
+      "facil":["a","s","d","f"],
+      "medio":["qaz","wsx","edc","rfv"], sin cambios
+    },
+    */
+    dificultad = {
+      "facil":["a","s","d","f"],
+      "medio":["qaz","wsx","edc","rfv"],
+      "dificil":["Kiet","saKi","HieLo","AsKo","KrikO","AsLa"]
+    },
+    textoamostrar =[],
+    pos=0,
 //declaracion del boton y la lectura del usuario
     btnusuario = document.getElementById('btnusuario'),
     usuario = document.getElementById('usuario'),
@@ -16,7 +30,7 @@ let canvas = document.getElementById('canvas'),
 
     btnDificil= document.getElementById('btnDificil'),
     btnDificil.disabled=true,
-    
+   
 //ocultacion de la entrada hasta que se seleccione la dificultad
     entrada= document.getElementById('Entrada'),
     entrada.disabled=true,
@@ -39,7 +53,15 @@ let canvas = document.getElementById('canvas'),
       } )
 
       btnFacil.addEventListener( 'click', function(){
-      
+        textoamostrar = dificultad.facil
+        context.fillText(textoamostrar[pos],10,50)
+
+        context.font = '20px serif';
+        context.fillText(textoamostrar[0],10,50)
+        context.fillText(textoamostrar[1],70,50)
+        context.fillText(textoamostrar[2],130,50)
+        context.fillText(textoamostrar[3],190,50)
+        
         //desbloqueamos la entrada
        entrada.disabled=false
 
@@ -58,6 +80,12 @@ let canvas = document.getElementById('canvas'),
        btnFacil.disabled=true,
        btnDificil.disabled=true
 
+       textoamostrar = dificultad.medio
+       context.font = '20px serif';
+       context.fillText(textoamostrar[0],10,50)
+       context.fillText(textoamostrar[1],70,50)
+       context.fillText(textoamostrar[2],130,50)
+       context.fillText(textoamostrar[3],190,50)
       } )
 
       btnDificil.addEventListener( 'click', function(){
@@ -69,8 +97,34 @@ let canvas = document.getElementById('canvas'),
        btnFacil.disabled=true,
        btnMedio.disabled=true
 
+       textoamostrar = dificultad.dificil
+       context.font = '20px serif';
+       context.fillText(textoamostrar[0],10,50)
+       context.fillText(textoamostrar[1],90,50)
+       context.fillText(textoamostrar[2],170,50)
+       context.fillText(textoamostrar[3],250,50)
+       context.fillText(textoamostrar[4],330,50)
+
+
       } )
     
       btnReinicio.addEventListener( 'click', function (){
         location.reload();
       } )
+      
+      btnFacil.addEventListener( 'click', function(){
+      //const Facil = "Este Texto es el facil si no"
+       
+      } )
+      
+      /* window.addEventListener( 'keydown', function(e){
+      
+       let tecla = e.code.split("Key")
+      console.log(textoamostrar[pos],tecla[1])
+      if(textoamostrar[pos]==tecla[1].toLowerCase()){
+        console.log("teclacorrecta")
+      }else{
+        console.log("teclaincorrecta")
+      }
+       })
+       */
